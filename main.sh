6e54b985323a2cab3dac16e8ee5a7a39bce37732
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2025-08-17 17:02:20 -0500

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Updates
+sudo apt update
+sudo apt upgrade
+
+# Fire wall
+sudo apt install ufw -y
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw enable
+# File Scanning
+read -p "Do you want to scan home directories for files? (y/n): " answer
+
+answer=${answer,,}
+
+if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+    read -p "Enter file extensions to search for (separated by space, or press Enter for all files): " -a extensions
+
+    ./scan_home_files.sh "${extensions[@]}"
+else
+    echo "Skipping home directory scan."
+fi
